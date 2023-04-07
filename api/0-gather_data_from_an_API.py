ebf0baac14f3610647d2864e7f429daa2d2d1820
root <root@DESKTOP-ISQ43BP>
2023-04-07 14:23:27 -0500

    upgrade

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,28 +1,25 @@
 #!/usr/bin/python3
 '''returns information about his/her TODO list progress.'''
+import pandas as pd
 import requests
-from sys import argv
-
+import sys
+'''data analysis'''
 if __name__ == '__main__':
-    API_URL = 'https://jsonplaceholder.typicode.com'
-    user_id = argv[1]
-    response = requests.get(
-        f'{API_URL}/users/{user_id}/todos',
-        params={'_expand': 'user'}
-    )
+    reponse = requests.get('https://jsonplaceholder.typicode.com/todos/')
+    '''data analysis'''
+    data = pd.read_json(reponse.text)
+    total_number_of_task = len(data[data['userId'] == int(sys.argv[1])])
+    completed = len(data[(data['userId'] == int(
+        sys.argv[1])) & (data['completed'] == True)])
 
-    if response.status_code == 200:
-        data = response.json()
-        name = data[0]['user']['name']
-        tasks_ok = [task for task in data if task['completed']]
-        n_task_ok = len(tasks_ok)
-        total_task = len(data)
+    reponse2 = requests.get('https://jsonplaceholder.typicode.com/users/')
+    data2 = pd.read_json(reponse2.text)
+    employee_name = data2[data2['id'] == int(sys.argv[1])]['name'].values[0]
+    task_title = list(data[(data['userId'] == int(sys.argv[1])) & (
+        data['completed'] == True)]['title'])
 
-        first_str = f"Employee {name} is done with tasks"
+    print("Employee {} is done with tasks({}/{}):".format(employee_name,
+          completed, total_number_of_task))
 
-        print(f"{first_str}({n_task_ok}/{total_task}):")
-        for task in tasks_ok:
-            print(f"\t {task['title']}")
-
-    else:
-        print(f"Error: {response.status_code}")
+    for i in task_title:
+        print("\t {}".format(i))
